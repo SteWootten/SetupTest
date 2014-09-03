38d433cf0b191d35011e30b73e6bec6f4269b314
Ste Wootten <stewootten@gmail.com>
2014-09-03 20:21:45 +0100

    Added another comment again

--- app/src/main/java/com/example/ste/setuptest/MainActivity.java
+++ app/src/main/java/com/example/ste/setuptest/MainActivity.java
@@ -21,6 +21,8 @@ import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
+    // My new comment
+
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
